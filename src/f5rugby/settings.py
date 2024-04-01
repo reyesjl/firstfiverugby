e44976886e1d06bbfb550355c598366f4c0d3070
reyesjl <reyesjl.nine@hotmail.com>
2024-03-31 21:41:34 -0400

    remove default database config from settings

--- src/f5rugby/settings.py
+++ src/f5rugby/settings.py
@@ -87,18 +87,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'f5rugby.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
