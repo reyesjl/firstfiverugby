172e329f07a365b5cb0db4f58286d6153b7f96ee
reyesjl <reyesjl.nine@hotmail.com>
2024-04-02 09:41:00 -0400

    webhook for uato_update from github;

--- src/f5rugby/urls.py
+++ src/f5rugby/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('camps/', views.camps, name='camps'),
     path('health/', views.health, name='health'),
+    path('deploy-webhook/', views.webhook_handler, name='deploy_webhook'),
 ]
--- src/f5rugby/views.py
+++ src/f5rugby/views.py
@@ -1,8 +1,10 @@
 # f5rugby/views.py
 # author: jose
-# date: April 1st, 2024
+# date: April 02, 2024
 
+from django.http import HttpResponse
 from django.shortcuts import render
+import subprocess
 
 def index(request):
     return render(request, 'f5rugby/index.html', context={})
@@ -12,3 +14,11 @@ def camps(request):
 
 def health(request):
     return render(request, 'f5rugby/health.html', context={})
+
+def webhook_handler(request):
+    if request.method == 'POST':
+        # Execute git pull
+        subprocess.Popen(['/home/reyesjl/django_sites/auto_update.sh'])
+        return HttpResponse('Webhook received', status=200)
+    else:
+        return HttpResponse('Method not allowed', status=405)
