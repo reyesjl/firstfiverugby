7f26db9814138b247a4c5aa8681be439a0539041
reyesjl <reyesjl.nine@hotmail.com>
2024-04-02 14:04:37 -0400

    add admin config for camps;

--- src/camp/admin.py
+++ src/camp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Camp
 
-# Register your models here.
+class CampAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date')
+    search_fields = ('title',)  # Enable search by camp name
+    list_filter = ('tags',)  # Enable filtering by tags
+
+admin.site.register(Camp, CampAdmin)
