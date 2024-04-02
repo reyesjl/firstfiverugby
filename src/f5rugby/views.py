49142f7b118eb18572614217f75143617e89484d
reyesjl <reyesjl.nine@hotmail.com>
2024-04-01 22:35:49 -0400

    add camps, link homepage; and static cmaps to camps page;

--- src/f5rugby/views.py
+++ src/f5rugby/views.py
@@ -1,8 +1,11 @@
 # f5rugby/views.py
 # author: jose
-# date: Friday, March 29th 2024
+# date: April 1st, 2024
 
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'f5rugby/index.html', context={})
+
+def camps(request):
+    return render(request, 'f5rugby/camps.html', context={})
