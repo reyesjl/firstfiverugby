dffb96fe7c63e28a0cc2bf3cb41160bc0d2a9f4e
reyesjl <reyesjl.nine@hotmail.com>
2024-04-02 12:27:33 -0400

    added logging statements;

--- src/f5rugby/views.py
+++ src/f5rugby/views.py
@@ -2,10 +2,14 @@
 # author: jose
 # date: April 02, 2024
 
+import logging
 import subprocess
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+
+# Configure logging
+logger = logging.getLogger(__name__)
 
 def index(request):
     return render(request, 'f5rugby/index.html', context={})
@@ -19,8 +23,16 @@ def health(request):
 @csrf_exempt
 def webhook_handler(request):
     if request.method == 'POST':
-        # Execute git pull
-        subprocess.Popen(['/home/reyesjl/django_sites/auto_update.sh'])
+        logger.info('Webhook received')
+        try:
+            # Execute git pull
+            subprocess.Popen(['/home/reyesjl/django_sites/auto_update.sh'])
+            logger.info('Git pull command executed')
+        except Exception as e:
+            logger.error('Error executing git pull command: %s' % str(e))
+            return HttpResponse('Error executing git pull command', status=500)
+        
         return HttpResponse('Webhook received', status=200)
     else:
+        logger.warning('Method not allowed')
         return HttpResponse('Method not allowed', status=405)
