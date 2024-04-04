d8c53497ef741f2ee5e9a5fcf0fefcfaf048ec52
reyesjl <reyesjl.nine@hotmail.com>
2024-04-04 13:59:06 -0400

    add fitness plans; add route to download file for fitness plans;

--- src/health/urls.py
+++ src/health/urls.py
@@ -6,4 +6,5 @@ app_name = 'health'
 urlpatterns = [
     path('', views.index, name='index'),
     path('fitness-evaluation/', views.submit_fitness_evaluation, name='fitness_evaluation'),
+    path('download-plan/<int:plan_id>/', views.download_plan_file, name='download_plan_file'),
 ]
--- src/health/views.py
+++ src/health/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib import messages
 from .models import HealthPlan
 from .forms import FitnessEvaluationForm
@@ -20,3 +21,12 @@ def submit_fitness_evaluation(request):
     else:
         form = FitnessEvaluationForm()
     return render(request, 'health/submit_fitness_evaluation.html', {'form': form})
+
+def download_plan_file(request, plan_id):
+    health_plan = get_object_or_404(HealthPlan, pk=plan_id)
+    file_path = health_plan.file.path
+    
+    with open(file_path, 'rb') as f:
+        response = HttpResponse(f.read(), content_type='application/octet-stream')
+        response['Content-Disposition'] = f'attachment; filename="{health_plan.file.name}"'
+        return response
