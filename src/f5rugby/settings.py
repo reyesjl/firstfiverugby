a0ce0081f059085e63a9480091c98279042bafb3
reyesjl <reyesjl.nine@hotmail.com>
2024-04-22 19:20:33 -0400

    alter media path format;

--- src/f5rugby/settings.py
+++ src/f5rugby/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media Storage configuration
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = '/media/'
 
 # Default primary key field type
