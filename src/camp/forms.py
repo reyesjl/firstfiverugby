bf601c0a5ec03456bb85f69134ee8866a24fc7e9
reyesjl <reyesjl.nine@hotmail.com>
2024-04-22 20:18:14 -0400

    add date widget to both forms;

--- src/camp/forms.py
+++ src/camp/forms.py
@@ -5,6 +5,9 @@ class PlayerRegistrationForm(forms.ModelForm):
     class Meta:
         model = GeneralRegistration
         fields = ['first_name', 'last_name', 'email', 'club', 'position', 'birthday', 'emergency_contact_first_name', 'emergency_contact_last_name', 'emergency_contact_email', 'emergency_contact_phone', 'photo_release_form', 'liability_waiver']
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'}),
+        }
 
 class CoachRegistrationForm(forms.ModelForm):
     class Meta:
@@ -15,6 +18,9 @@ class ModifyPlayerRegistrationForm(forms.ModelForm):
     class Meta:
         model = GeneralRegistration
         fields = ['first_name', 'last_name', 'email', 'club', 'position', 'birthday', 'emergency_contact_first_name', 'emergency_contact_last_name', 'emergency_contact_email', 'emergency_contact_phone', 'has_paid']
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'}),
+        }
 
 class ModifyCoachRegistrationForm(forms.ModelForm):
     class Meta:
