0a42459fa1c41621a9f45db5f70ccf11bbbfedb0
reyesjl <reyesjl.nine@hotmail.com>
2024-05-06 14:06:16 -0400

    fix nav; update preview;

--- src/static/js/components/navigation.js
+++ src/static/js/components/navigation.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
     menuToggle.addEventListener('click', function() {
         menuWrapper.classList.toggle('show-menu');
         if (menuWrapper.classList.contains('show-menu')) {
-            menuToggle.querySelector('.text-white').textContent = 'Close';
+            menuToggle.textContent = 'Close';
             document.body.classList.add('disable-scroll');
         } else {
-            menuToggle.querySelector('.text-white').textContent = 'Menu';
+            menuToggle.textContent = 'Menu';
             document.body.classList.remove('disable-scroll');
         }
     });
